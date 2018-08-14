a2cbf38de1de6a2f412e2cf3dc60f644d5ffe081
ahmed atta <ahmedatta3322@gmail.com>
2018-08-14 17:27:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function destructivelyRemoveFirstKitten(){
 function appendKitten(name){
   return [...kittens,name]
 }
-function appendKitten(name){
-  return [...kittens,name]
+function prependKitten(name){
+  return [name,...kittens]
 }
